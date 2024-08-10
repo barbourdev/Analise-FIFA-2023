dd927981751a2f4d48ada059a492d9ec6bba7743
barbourdev <felipe.barbourf@gmail.com>
2024-08-09 22:09:14 -0300

    Configuração da pagina de players

---
+++ pages/2_Players.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config( ## Configurações da página web do streamlit
+    page_title="Players - FIFA", ## Titulo da página
+    layout="wide", ## Se a página irá carregar com o tamanho 100%
+    initial_sidebar_state="expanded" ## Se a sidebar irá carregar aberta ou fechada
+)
